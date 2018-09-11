e57e2f099d3f0c092667dbfacca7642bc649091c
KarolineMJ <karoline_mj@hotmail.com>
2018-09-11 09:45:52 +0200

    viser navn

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 "use strict";
 
-//fetch the json 
+//read JSON and fetch 
 fetch("classlist.json")
-    .then(e => e.json())
+    .then(result => result.json())
     .then(studentList)
+
 
 //create an array of objects 
 const students = [];
+let houseArray = [];
 
 //create single object 
 const student = {
     firstName: "",
     lastName: "",
+    houseName: "",
     toString() {
         return this.firstName + " " + this.lastName;
     },
@@ -20,18 +23,33 @@ const student = {
         this.firstName = fullName.substring(0, firstSpace);
         this.lastName = fullName.substring(firstSpace + 1);
 
+    },
+    getHouse(h) {
+        this.houseName = h;
     }
 }
 
+//forEach student in house 
 function studentList(data) {
     console.table(data);
-    data.Gryffindor.forEach(showPerson);
+    Object.keys(data).forEach(gethouseName);
+    function gethouseName(houseName) {
+        console.log(houseName);
+    }
 }
+
+
+//create student object 
+
+//set name and house 
+
+//add/push to students array
+
 
 function showPerson(fullName) { //createStudent takes the splitName function, and splits the array into strings
     const newStudent = Object.create(student); //then create a new object
     newStudent.splitName(fullName);//splits the name - runs the fullName function
-
+    //newStudent.getHouse()
     students.push(newStudent); //puts it into an empty array
 }
 
